4eed3da3437f14f73633f7bed38578e88421eed3
Ang Jun Ray <angjunray@live.com>
2021-02-25 23:23:16 +0800

    Emailing done!

--- notifier/main.py
+++ notifier/main.py
@@ -1,12 +1,26 @@
 import firebase_admin
+import ssl
+import smtplib
 from firebase_admin import credentials, firestore
 cred = credentials.Certificate('notifier/ServiceAccountKey.json')
 
 default_app = firebase_admin.initialize_app(cred)
 dataBase = firestore.client()
 
-doc_ref = dataBase.collection(u'sampleData').document(u'lol')
-doc_ref.set({
-    u'quote': 100,
-    u'lol': 'meh',
-})
+smtp_server = 'smtp.gmail.com'
+port = 465
+sender = 'stamfordunoffical@gmail.com'
+receiver = 'xiaonianhe@hotmail.com'
+password = input("Password:")
+message = """
+Subject: Yay it works? (:D)
+
+This message was sent from Python!
+"""
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender, password)
+    # send email
+    server.sendmail(sender, receiver, message)
